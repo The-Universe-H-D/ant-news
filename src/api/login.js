e58052610e287741ab428e4bddf8b32258e61ea2
myid-hana <sosqkrgksml@gmail.com>
2021-04-20 13:43:06 +0900

    가짜 api 만들기

---
+++ src/api/login.js
@@ -0,0 +1,16 @@
+const users = [
+	{
+		id: 1,
+		userId: 'sosqkrgksml',
+		userPw: '111',
+		userName: '박하나'
+	},
+	{
+		id: 2,
+		userId: 'whdk999',
+		userPw: '111',
+		userName: '김동현'
+	}
+];
+
+export default users;
