33961e85b78158af14028e195c99bcd16ebc4664
myid-hana <sosqkrgksml@gmail.com>
2021-04-23 09:55:11 +0900

    그래프 차트 넣기 연습

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -3,19 +3,56 @@ import Button from './Button';
 import '../assets/scss/HomeComponent.scss';
 import LogoImg from './LogoImg';
 import SearchInput from './SearchInput';
+import { Link } from 'react-router-dom';
+import { Line } from 'react-chartjs-2';
 
 function HomeComponent({ onSetLogout, loginState }) {
+	const options = {
+		legend: {
+			display: false // label 보이기 여부
+		},
+		scales: {
+			yAxes: [
+				{
+					ticks: {
+						min: 0, // y축 스케일에 대한 최소값 설정
+						stepSize: 1 // y축 그리드 한 칸당 수치
+					}
+				}
+			]
+		},
+
+		// false : 사용자 정의 크기에 따라 그래프 크기가 결정됨.
+		// true : 크기가 알아서 결정됨.
+		maintainAspectRatio: false
+	};
+	const data = {
+		// 각 막대별 라벨
+		labels: ['1번 막대', '2번 막대', '3번 막대'],
+		datasets: [
+			{
+				borderWidth: 1, // 테두리 두께
+				data: [1, 2, 3], // 수치
+				backgroundColor: ['yellow', 'red', 'green'] // 각 막대 색
+			}
+		]
+	};
 	return (
 		<div>
 			<header>
 				{!loginState ? (
 					<div className="login_btn_box">
-						<Button>로그인</Button>
-						<Button>회원가입</Button>
+						<Link to="/login">
+							<Button>로그인</Button>
+						</Link>
+						<Link to="/join">
+							<Button>회원가입</Button>
+						</Link>
 					</div>
 				) : (
 					<div className="logout_btn_box">
-						<Button onClick={onSetLogout}>로그아웃</Button>
+						<b>세션에서 사용자 정보 가져오기</b>
+						<button onClick={onSetLogout}>로그아웃</button>
 					</div>
 				)}
 			</header>
@@ -23,6 +60,9 @@ function HomeComponent({ onSetLogout, loginState }) {
 				<LogoImg />
 				<SearchInput />
 			</section>
+			<div style={{ height: '400px' }}>
+				<Line data={data} options={options} height={300} />
+			</div>
 		</div>
 	);
 }
