0bdb8d558fb0e65fb2fc374e9db78c7e20589499
myid-hana <sosqkrgksml@gmail.com>
2021-04-25 13:38:37 +0900

    로그인 정보 세션에 저장

--- src/components/GoogleLoginComponent.js
+++ src/components/GoogleLoginComponent.js
@@ -3,38 +3,41 @@ import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
 function GoogleLoginComponent() {
 	const [loginState, setLoginState] = useState(false);
-	const [userName, setUserName] = useState('');
-	const responseGoogle = response => {
-		console.log(response);
-		setUserName(response.gt.Te);
+	const userName = loginState ? window.sessionStorage.getItem('userName') : '';
+	const onLoginSuccess = res => {
+		console.log(res);
+		window.sessionStorage.setItem('userName', res.gt.Te);
 		setLoginState(true);
 	};
-	const onLogoutSuccess = response => {
-		console.log(response);
+	const onLoginFailure = res => {
+		console.log(res);
+	};
+	const onLogoutSuccess = res => {
+		console.log(res);
+		window.sessionStorage.clear();
 		setLoginState(false);
 	};
-	return (
-		<div>
-			{loginState ? (
-				<>
-					<b>{userName}님 환영합니다.</b>
-					<GoogleLogout
-						clientId="379175647400-vb7hlqes9vi20lach33v4o8quihmaauc.apps.googleusercontent.com"
-						buttonText="Logout"
-						onLogoutSuccess={onLogoutSuccess}
-					/>
-				</>
-			) : (
-				<GoogleLogin
+	if (loginState)
+		return (
+			<>
+				<b>{userName}님 환영합니다.</b>
+				<GoogleLogout
 					clientId="379175647400-vb7hlqes9vi20lach33v4o8quihmaauc.apps.googleusercontent.com"
-					buttonText="GoogleLogin"
-					onSuccess={responseGoogle}
-					onFailure={responseGoogle}
-					cookiePolicy={'single_host_origin'}
+					buttonText="Logout"
+					onLogoutSuccess={onLogoutSuccess}
 				/>
-			)}
-		</div>
-	);
+			</>
+		);
+	if (!loginState)
+		return (
+			<GoogleLogin
+				clientId="379175647400-vb7hlqes9vi20lach33v4o8quihmaauc.apps.googleusercontent.com"
+				buttonText="Google Login"
+				onSuccess={onLoginSuccess}
+				onFailure={onLoginFailure}
+				cookiePolicy={'single_host_origin'}
+			/>
+		);
 }
 
 export default GoogleLoginComponent;
