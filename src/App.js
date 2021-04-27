032cd7bb90d11512a789d88b7bcfe8c969eefb90
myid-hana <sosqkrgksml@gmail.com>
2021-04-27 17:53:55 +0900

    라우터 제거하고 1개의 url로만 변경

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import HomeContainer from './containers/HomeContainer';
-import { Route, Switch } from 'react-router';
 import SearchContainer from './containers/SearchContainer';
 
 function App() {
 	return (
 		<div className="App">
-			<Switch>
-				<Route path="/" component={HomeContainer} exact />
-				<Route path="/search" component={SearchContainer} />
-				<Route
-					render={({ location }) => (
-						<div>
-							<h2>이 페이지는 존재하지 않습니다.</h2>
-							<p>{location.pathname}</p>
-						</div>
-					)}
-				/>
-			</Switch>
+			<HomeContainer />
+			<SearchContainer />
 		</div>
 	);
 }
