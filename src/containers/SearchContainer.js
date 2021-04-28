c1f873161e6768645caf37c643c85b160b93ce5a
myid-hana <sosqkrgksml@gmail.com>
2021-04-28 12:48:49 +0900

    컨테이너에 디스패치 추가

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -2,28 +2,29 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import SearchInput from '../components/SearchInput';
 import SearchPage from '../components/SearchPage';
-import { getNewsApiAsync, getStockApiAsync } from '../modules/getAPI';
+import { getNewsList, getStockChart, getStockSummary } from '../modules/getAPI';
 import initialState from '../modules/getAPI';
 
 function SearchContainer() {
-	const { data, loading } = useSelector(state => state.getApiReducer || initialState);
-	const newsList = data ? data.data.newsList : [];
+	const { stock, news } = useSelector(state => state.getApiReducer || initialState);
+	const newsList = news.data ? news.data.data.newsList : [];
 	const dispatch = useDispatch();
-	const onGetApi = (value, history) => {
-		dispatch(getNewsApiAsync(value));
-		dispatch(getStockApiAsync(value));
+	const onGetApi = value => {
+		dispatch(getNewsList(value));
+		//dispatch(getStockChart(value));
+		//dispatch(getStockSummary(value));
 	};
 
-	useEffect(() => {}, [data, loading]);
+	useEffect(() => {}, []);
 
-	if (loading) return <div style={{ display: 'flex', marginTop: '15%', justifyContent: 'center' }}>로딩중...</div>;
-	if (!data)
+	if (news.loading || stock.loading)
+		return <div style={{ display: 'flex', marginTop: '15%', justifyContent: 'center' }}>로딩중...</div>;
+	if (!stock.data && !news.data)
 		return (
 			<div style={{ display: 'flex', marginTop: '15%', justifyContent: 'center' }}>
 				<SearchInput onGetApi={onGetApi} />
 			</div>
 		);
-	if (newsList === []) return <div>검색 결과가 없습니다.</div>;
 	return (
 		<div>
 			<SearchPage onGetApi={onGetApi} newsList={newsList} />
