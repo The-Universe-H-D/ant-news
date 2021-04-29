3b51142c548054edf1a0c0b83b78f1f35f4d113e
myid-hana <sosqkrgksml@gmail.com>
2021-04-29 11:25:03 +0900

    StockSummary Style 수정

--- src/components/StockSummary.js
+++ src/components/StockSummary.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../assets/scss/StockSummary.scss';
 
 function StockSummary({ summaryData }) {
 	const {
@@ -17,79 +18,89 @@ function StockSummary({ summaryData }) {
 		forwardDividendAndYield,
 		exdividendDate,
 		yearTargetEstimate,
-		earningDate
+		earningDate,
+		name,
+		website
 	} = summaryData;
 	return (
-		<div>
-			<ul>
-				<li>
-					<span>Previous Close</span>
-					<b>{previousClose || 'N/A'}</b>
-				</li>
-				<li>
-					<span>Open</span>
-					<b>{open || 'N/A'}</b>
-				</li>
-				<li>
-					<span>Bid</span>
-					<b>{bid || 'N/A'}</b>
-				</li>
-				<li>
-					<span>Ask</span>
-					<b>{ask || 'N/A'}</b>
-				</li>
-				<li>
-					<span>Day's Range</span>
-					<b>{dayRange || 'N/A'}</b>
-				</li>
-				<li>
-					<span>52 Week Range</span>
-					<b>{yearRange || 'N/A'}</b>
-				</li>
-				<li>
-					<span>Volume</span>
-					<b>{volume || 'N/A'}</b>
-				</li>
-				<li>
-					<span>Avg. Volume</span>
-					<b>{averageVolume || 'N/A'}</b>
-				</li>
-			</ul>
-			<ul>
-				<li>
-					<span>Market Cap</span>
-					<b>{marketCapitalization || 'N/A'}</b>
-				</li>
-				<li>
-					<span>Beta (5Y Monthly)</span>
-					<b>{beta5YMonthly || 'N/A'}</b>
-				</li>
-				<li>
-					<span>PE Ratio (TTM)</span>
-					<b>{priceEarningsRatio || 'N/A'}</b>
-				</li>
-				<li>
-					<span>EPS (TTM)</span>
-					<b>{earningPerShare || 'N/A'}</b>
-				</li>
-				<li>
-					<span>Earnings Date</span>
-					<b>{earningDate || 'N/A'}</b>
-				</li>
-				<li>
-					<span>Forward Dividend &#38; Yield</span>
-					<b>{forwardDividendAndYield || 'N/A'}</b>
-				</li>
-				<li>
-					<span>Ex-Dividend Date</span>
-					<b>{exdividendDate || 'N/A'}</b>
-				</li>
-				<li>
-					<span>1y Target Est</span>
-					<b>{yearTargetEstimate || 'N/A'}</b>
-				</li>
-			</ul>
-		</div>
+		<>
+			<div className="stock-name">
+				<b className="name">{name}</b>
+				<span className="website">
+					<a href={website}>{website}</a>
+				</span>
+			</div>
+			<div className="StockSummary">
+				<ul className="sum-1">
+					<li>
+						<span>Previous Close</span>
+						<b>{previousClose || 'N/A'}</b>
+					</li>
+					<li>
+						<span>Open</span>
+						<b>{open || 'N/A'}</b>
+					</li>
+					<li>
+						<span>Bid</span>
+						<b>{bid || 'N/A'}</b>
+					</li>
+					<li>
+						<span>Ask</span>
+						<b>{ask || 'N/A'}</b>
+					</li>
+					<li>
+						<span>Day's Range</span>
+						<b>{dayRange || 'N/A'}</b>
+					</li>
+					<li>
+						<span>52 Week Range</span>
+						<b>{yearRange || 'N/A'}</b>
+					</li>
+					<li>
+						<span>Volume</span>
+						<b>{volume || 'N/A'}</b>
+					</li>
+					<li>
+						<span>Avg. Volume</span>
+						<b>{averageVolume || 'N/A'}</b>
+					</li>
+				</ul>
+				<ul className="sum-2">
+					<li>
+						<span>Market Cap</span>
+						<b>{marketCapitalization || 'N/A'}</b>
+					</li>
+					<li>
+						<span>Beta (5Y Monthly)</span>
+						<b>{beta5YMonthly || 'N/A'}</b>
+					</li>
+					<li>
+						<span>PE Ratio (TTM)</span>
+						<b>{priceEarningsRatio || 'N/A'}</b>
+					</li>
+					<li>
+						<span>EPS (TTM)</span>
+						<b>{earningPerShare || 'N/A'}</b>
+					</li>
+					<li>
+						<span>Earnings Date</span>
+						<b>{earningDate || 'N/A'}</b>
+					</li>
+					<li>
+						<span>Forward Dividend &#38; Yield</span>
+						<b>{forwardDividendAndYield || 'N/A'}</b>
+					</li>
+					<li>
+						<span>Ex-Dividend Date</span>
+						<b>{exdividendDate || 'N/A'}</b>
+					</li>
+					<li>
+						<span>1y Target Est</span>
+						<b>{yearTargetEstimate || 'N/A'}</b>
+					</li>
+				</ul>
+			</div>
+		</>
 	);
 }
 
