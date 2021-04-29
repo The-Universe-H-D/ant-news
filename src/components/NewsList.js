a57ad31167c1ab74d3f78f5bfdcccb44dcb17e5a
myid-hana <sosqkrgksml@gmail.com>
2021-04-29 13:38:53 +0900

    Newslist 디자인 수정

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -4,12 +4,15 @@ import '../assets/scss/NewsList.scss';
 function NewsList({ newsList }) {
 	return (
 		<div className="NewsList">
+			<b className="news-title">News</b>
 			<ul>
 				{newsList.map(news => (
 					<li key={news.id} id={news.id} className="lists">
 						<img className="newsImg" alt="news thumbnail" src={news.thumbnail}></img>
 						<a className="links">
-							<span>{news.title}</span>
+							<p className="title">{news.title}</p>
+							<span className="date">{news.pubDate}</span>
+							<span className="provider">{news.provider}</span>
 						</a>
 					</li>
 				))}
